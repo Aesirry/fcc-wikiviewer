52890a3a9adc444514ff9d0f39ae2ff0be918716
Joe Beltrami <jpbeltrami@gmail.com>
2017-02-17 22:02:45 +0800

    added some js that still needs work

--- scripts.js
+++ scripts.js
@@ -1,5 +1,18 @@
+var muhquery;
+
+function findArticle(query){
+  $.getJSON("https://en.wikipedia.org/w/api.php?action=query&format=json&generator=search&grnnamespace=0&prop=extracts&exlimit=max&explaintext&exintro&gsrsearch=" + query + "&callback=?",
+function(data) {
+  for(var i = 0; i < 10; i++) {
+    var stuff = $("<p></p>").text(data.search[i].snippet);
+    $("#search-area").append(stuff);
+  }
+});
+}
+
 $(document).ready(function(){
-  $("#submit").click(function() {
-    $("#search")
+  $("button").click(function() {
+    query = $("#search").val();
+    findArticle(query);
   });
 });
